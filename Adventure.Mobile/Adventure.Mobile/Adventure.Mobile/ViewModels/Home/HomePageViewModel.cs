e055dda98196bab1078e7abc1175ec38cb7d96a1
Teddy <teddy.duri@ec.co.za>
2019-09-16 22:59:01 +0200

    Carousel View Home page

--- Adventure.Mobile/Adventure.Mobile/Adventure.Mobile/ViewModels/Home/HomePageViewModel.cs
+++ Adventure.Mobile/Adventure.Mobile/Adventure.Mobile/ViewModels/Home/HomePageViewModel.cs
@@ -3,19 +3,48 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using Xamarin.Forms;
 
 namespace Adventure.Mobile.ViewModels.Home
 {
-    public class HomePageViewModel : BindableBase
-    {
-        public ObservableCollection Monkeys { get; set; }
-        public ObservableCollection<Grouping> MonkeysGrouped { get; set; }
 
-        public ObservableCollection Zoos { get; set; }
+    public class HomePageViewModel : BindableBase, INotifyPropertyChanged
+    {
         public HomePageViewModel()
         {
 
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<Section> _sections;
+        public ObservableCollection<Section> Sections
+        {
+            get
+            {
+                return _sections;
+            }
+            set
+            {
+                if (_sections != value)
+                {
+                    _sections = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("Sections"));
+                }
+            }
+        }
+        private void OnPropertyChanged(PropertyChangedEventArgs eventArgs)
+        {
+            PropertyChanged?.Invoke(this, eventArgs);
+        }
     }
+
+    public class Section
+    {
+        public string Header { get; set; }
+        public FormattedString Content { get; set; }
+        public string Emoji { get; set; }
+    };
 }
--- Adventure.Mobile/Adventure.Mobile/Adventure.Mobile/Views/Home/HomePage.xaml.cs
+++ Adventure.Mobile/Adventure.Mobile/Adventure.Mobile/Views/Home/HomePage.xaml.cs
@@ -1,12 +1,121 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.ObjectModel;
+using Adventure.Mobile.ViewModels.Home;
+using Xamarin.Forms;
 
 namespace Adventure.Mobile.Views.Home
 {
     public partial class HomePage : ContentPage
     {
+        public HomePageViewModel VM { get; set; }
         public HomePage()
         {
             InitializeComponent();
+            VM = new HomePageViewModel();
+            VM.Sections = new ObservableCollection<Section>
+            {
+                new Section
+                {
+                    Header = "Thank you for participating in the CarouselView Challenge!",
+                    Content = new FormattedString
+                    {
+                        Spans =
+                        {
+                            new Span
+                            {
+                                Text = "Your feedback is highly valuable for helping to drive CarouselView's development and highlight functionality you need most." + Environment.NewLine + Environment.NewLine
+                            },
+                            new Span
+                            {
+                                Text = "(This is a CarouselView, so swipe to the left to keep reading!)",
+                                FontAttributes = FontAttributes.Italic
+                            }
+                        }
+                    }
+                },
+                new Section
+                {
+                    Header = "What to Do",
+                    Content = new FormattedString
+                    {
+                        Spans =
+                        {
+                            new Span
+                            {
+                                Text = "Pick an app and reproduce a part of it where you can use the CarouselView." + Environment.NewLine + Environment.NewLine
+                            },
+                            new Span
+                            {
+                                Text = "This could be part of an app you've developed using Xamarin.Forms, or anything you'd like. Your choice!"
+                            }
+                        }
+                    }
+                },
+                new Section
+                {
+                    Header = "Create a PR and Give Feedback",
+                    Content = new FormattedString
+                    {
+                        Spans =
+                        {
+                            new Span
+                            {
+                                Text = "When you're finished, open a PR with your work, and feel free to share screenshots." + Environment.NewLine + Environment.NewLine
+                            },
+                            new Span
+                            {
+                                Text = "If you participated in the CollectionView Challenge, you'll be familiar with the feedback we'd like you to provide:"
+                            }
+                        }
+                    }
+                },
+                new Section
+                {
+                    Header = "1. What went well?",
+                    Content = new FormattedString
+                    {
+                        Spans =
+                        {
+                            new Span
+                            {
+                                Text = "Was the CarouselView easy to use? Were you able to accomplish what you set out to?" + Environment.NewLine + Environment.NewLine
+                            }
+                        }
+                    },
+                    Emoji = "😀"
+                },
+                new Section
+                {
+                    Header = "2. What didn't go well?",
+                    Content = new FormattedString
+                    {
+                        Spans =
+                        {
+                            new Span
+                            {
+                                Text = "Be as honest and detailed as possible." + Environment.NewLine + Environment.NewLine
+                            }
+                        }
+                    },
+                    Emoji = "🙁"
+                },
+                new Section
+                {
+                    Header = "3. What's missing?",
+                    Content = new FormattedString
+                    {
+                        Spans =
+                        {
+                            new Span
+                            {
+                                Text = "Let us know about any functionality you didn't have access to and its impact on you, or anything else you'd like to see." + Environment.NewLine + Environment.NewLine
+                            }
+                        }
+                    },
+                    Emoji = "🤔"
+                },
+            };
+            BindingContext = VM;
         }
     }
 }
