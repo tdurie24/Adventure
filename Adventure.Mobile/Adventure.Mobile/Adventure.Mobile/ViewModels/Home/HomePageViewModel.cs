519d274de3c47a7d9f6166de1ab5dc9bebfc638b
Teddy <teddy.duri@ec.co.za>
2019-09-16 07:40:15 +0200

    Carousel View for home page

--- Adventure.Mobile/Adventure.Mobile/Adventure.Mobile/ViewModels/Home/HomePageViewModel.cs
+++ Adventure.Mobile/Adventure.Mobile/Adventure.Mobile/ViewModels/Home/HomePageViewModel.cs
@@ -2,12 +2,17 @@
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace Adventure.Mobile.ViewModels.Home
 {
     public class HomePageViewModel : BindableBase
     {
+        public ObservableCollection Monkeys { get; set; }
+        public ObservableCollection<Grouping> MonkeysGrouped { get; set; }
+
+        public ObservableCollection Zoos { get; set; }
         public HomePageViewModel()
         {
 
