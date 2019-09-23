ac037aa7dfb9368d3f019d8ade8b1e9ceda49a91
Teddy <teddy.duri@ec.co.za>
2019-09-23 03:56:48 +0200

    API constants fix

--- Adventure.Mobile/Adventure.Mobile.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Adventure.Mobile.Repository
-{
-    public class Class1
-    {
-    }
-}
--- Adventure.Mobile/Adventure.Mobile/Adventure.Mobile/Constants/APIConstants.cs
+++ Adventure.Mobile/Adventure.Mobile/Adventure.Mobile/Constants/APIConstants.cs
@@ -7,13 +7,8 @@ namespace Adventure.Mobile.Constants
    public class APIConstants
     {
         public const string BaseApiUrl = "http://192.168.56.1:5000/";
-        public const string Holiday = "api/catalog/pies/";
-        public const string PiesOfTheWeekEndpoint = "api/catalog/piesoftheweek/";
-        public const string ShoppingCartEndpoint = "api/shoppingcart";
-        public const string AddShoppingCartItemEndpoint = "api/shoppingcart/";
-        public const string AddContactInfoEndpoint = "api/contact";
-        public const string PlaceOrderEndpoint = "api/order";
-        public const string RegisterEndpoint = "api/authentication/register";
+        public const string HolidaysEndpoint = "/api/holidays";
+        public const string EventsEndpoint = "api/events/";
         public const string AuthenticateEndpoint = "api/authentication/authenticate";
     }
 }
