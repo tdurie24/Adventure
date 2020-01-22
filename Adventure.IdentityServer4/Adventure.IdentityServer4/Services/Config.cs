b65b59d800321d8fe6cc328b873ffd9b097671e0
Teddy <tdurie24@gmail.com>
2020-01-22 14:52:39 +0200

    Identity Server4 reconfigurations and upgrade

---
+++ Adventure.IdentityServer4/Adventure.IdentityServer4/Services/Config.cs
@@ -0,0 +1,39 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adventure.IdentityServer4.Services
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("AdventureAPI", "Adventure API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client()
+                {
+                  ClientId = "client",
+                  ClientName  ="AdventureAPI",
+                  AllowedGrantTypes = GrantTypes.ClientCredentials,
+                  ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                  AllowedScopes  = { "AdventureAPI" }
+
+                }
+            };
+        }
+    }
+}
+
